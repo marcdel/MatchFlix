5c7a8e631d6f054680c6e74e8586ff1cb4c421e5
Marc Delagrammatikas <delagrammatikas@gmail.com>
2021-03-24 16:04:31 -0700

    add label view anchored to the bottom

--- MatchFlix/View/CardView.swift
+++ MatchFlix/View/CardView.swift
@@ -17,6 +17,19 @@ class CardView: UIView {
         return iv
     }()
 
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+
+        let attributedText = NSMutableAttributedString(string: "Jane Doe", attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy), .foregroundColor: UIColor.white])
+
+        attributedText.append(NSAttributedString(string: "  20", attributes: [.font: UIFont.systemFont(ofSize: 24), .foregroundColor: UIColor.white]))
+
+        label.attributedText = attributedText
+
+        return label
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -25,7 +38,10 @@ class CardView: UIView {
         clipsToBounds = true
 
         addSubview(imageView)
+        addSubview(infoLabel)
+
         imageView.fillSuperview()
+        infoLabel.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingLeft: 16, paddingBottom: 16, paddingRight: 16)
     }
 
     required init?(coder: NSCoder) {
